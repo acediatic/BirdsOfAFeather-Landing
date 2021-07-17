21f76add13639425d4292f283acf923304a2446c
Adam Sinclair <hello@adamsinclair.dev>
2021-07-17 12:19:14 +1200

    removed bounce register

--- src/utils/BounceRegisterOnce.js
+++ src/utils/BounceRegisterOnce.js
@@ -1,12 +1,6 @@
 function BounceRegisterOnce() {
   const registerInternal = document.getElementById("register-internal");
-  const ogClassname = registerInternal.className;
-
   registerInternal.scrollIntoView();
-  registerInternal.className += " animate-bounce"
-  setTimeout(() => {
-    registerInternal.className = ogClassname;
-  }, 1500);
 }
 
 export default BounceRegisterOnce;
